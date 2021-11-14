8d25fc2e5920fd228fc02bb857026910f6fec672
Szkaradd <94292546+Szkaradd@users.noreply.github.com>
2021-11-14 21:59:50 +0100

    Add files via upload

--- fuzzy.cc
+++ fuzzy.cc
@@ -1,20 +1,22 @@
-#include <algorithm>
+#include <iostream>
 #include "fuzzy.h"
+#include <vector>
 
-/*
-TriFuzzyNum num(1.25, 2.25, 3.25);
-TriFuzzyNum num(2.25, 3.25, 1.25);
-constexpr TriFuzzyNum num(1.25, 2.25, 3.25);
-constinit static TriFuzzyNum num(1.25, 2.25, 3.25);
-*/
+using std::vector;
+using std::ostream;
 
-constexpr TriFuzzyNum::TriFuzzyNum(real_t a, real_t b, real_t c) {
-    this->l = std::min(a, b, c);
-    this->u = std::max(a, b, c);
-    this->m =
-        l <= a <= u 
-        ? a
-        : l <= b <= u
-            ? b
-            : c;
+ostream &operator<<(ostream &os, const TriFuzzyNum &t) {
+    os << "("
+       << t.l
+       << ", "
+       << t.m
+       << ", "
+       << t.u
+       << ")";
+    return os;
 }
+
+
+
+
+
--- fuzzy.h
+++ fuzzy.h
@@ -1,23 +1,125 @@
-#ifndef _FUZZY_H
-#define _FUZZY_H
+#ifndef FUZZY_H
+#define FUZZY_H
 
-using real_t = long double;
+#include <iostream>
+#include <cmath>
+
+using real_t = double;
+using std::ostream;
 
 class TriFuzzyNum {
-    real_t l;
-    real_t m;
-    real_t u;
-
 public:
-    constexpr TriFuzzyNum(real_t a, real_t b, real_t c);
-    constexpr real_t lower_value();
-    constexpr real_t modal_value();
-    constexpr real_t upper_value();
+    constexpr TriFuzzyNum(const real_t &a, const real_t &b, const real_t &c) {
+        real_t values[] = {a, b, c};
+        std::sort(std::begin(values), std::end(values));
 
-    bool operator==(TriFuzzyNum other);
-    TriFuzzyNum operator+(TriFuzzyNum other);
-    TriFuzzyNum operator-(TriFuzzyNum other);
+        this->l = values[0];
+        this->m = values[1];
+        this->u = values[2];
+    }
 
-}
+    constexpr real_t lower_value() const {
+        return this->l;
+    }
 
-#endif // _FUZZY_H
+    constexpr real_t modal_value() const {
+        return this->m;
+    }
+
+    constexpr real_t upper_value() const {
+        return this->u;
+    }
+
+    constexpr TriFuzzyNum operator+(const TriFuzzyNum &that) const {
+        real_t new_l = this->l + that.l;
+        real_t new_m = this->m + that.m;
+        real_t new_u = this->u + that.u;
+        return TriFuzzyNum(new_l, new_m, new_u);
+    }
+
+    constexpr TriFuzzyNum operator-(const TriFuzzyNum &that) const {
+        real_t new_l = this->l - that.u;
+        real_t new_m = this->m - that.m;
+        real_t new_u = this->u - that.l;
+        return TriFuzzyNum(new_l, new_m, new_u);
+    }
+
+    constexpr TriFuzzyNum operator*(const TriFuzzyNum &that) const {
+        real_t new_l = this->l * that.l;
+        real_t new_m = this->m * that.m;
+        real_t new_u = this->u * that.u;
+        return TriFuzzyNum(new_l, new_m, new_u);
+    }
+
+    TriFuzzyNum operator+=(const TriFuzzyNum &that) {
+        this->l += that.l;
+        this->m += that.m;
+        this->u += that.u;
+        return *this;
+    }
+
+    TriFuzzyNum operator-=(const TriFuzzyNum &that) {
+        this->l -= that.u;
+        this->m -= that.m;
+        this->u -= that.l;
+        return *this;
+    }
+
+    TriFuzzyNum operator*=(const TriFuzzyNum &that) {
+        this->l *= that.l;
+        this->m *= that.m;
+        this->u *= that.u;
+        return *this;
+    }
+
+    constexpr int operator<=>(const TriFuzzyNum &that) const {
+        real_t z1 = this->u - this->l + sqrt(1 + (this->u - this->m) *
+                (this->u - this->m)) + sqrt(1 + (this->m - this->l) * (this->m - this->l));
+        real_t y1 = (this->u - this->l) / z1;
+        real_t x1 = ((this->u - this->l) * this->m + sqrt(1 + (this->u - this->m) * (this->u - this->m))
+                * this->l + sqrt(1 + (this->m - this->l) *(this->m - this->l)) * this->u) / z1;
+
+        real_t z2 = that.u - that.l + sqrt(1 + (that.u - that.m) *
+                (that.u - that.m)) + sqrt(1 + (that.m - that.l) * (that.m - that.l));
+        real_t y2 = (that.u - that.l) / z2;
+        real_t x2 = ((that.u - that.l) * that.m + sqrt(1 + (that.u - that.m) * (that.u - that.m))
+                * that.l + sqrt(1 + (that.m - that.l) *(that.m - that.l)) * that.u) / z2;
+
+        real_t first1 = (x1 - y1) / 2;
+        real_t first2 =(x2 - y2) / 2;
+        real_t second1 = 1 - y1;
+        real_t second2 = 1 - y2;
+        real_t third1 = this->m;
+        real_t third2 = that.m;
+
+        if (first1 > first2) return 1;
+        else if (first1 < first2) return -1;
+        else {
+            if (second1 > second2) return 1;
+            else if (second1 < second2) return -1;
+            else {
+                if (third1 > third2) return 1;
+                else if (third1 < third2) return -1;
+                else return 0;
+            }
+        }
+    }
+
+    constexpr bool operator==(const TriFuzzyNum &that) const {
+        return (this->l == that.l && this->m == that.m && this->u == that.u);
+    }
+
+    constexpr bool operator!=(const TriFuzzyNum &that) const {
+        return (this->l != that.l || this->m != that.m || this->u != that.u);
+    }
+
+    /*constexpr TriFuzzyNum crisp_number(real_t v) {
+        return TriFuzzyNum(v, v, v);
+    }*/
+
+private:
+    real_t l, m, u;
+    friend std::ostream& operator<<(std::ostream&, const TriFuzzyNum&);
+};
+
+#endif //FUZZY_H
